db203f1102ce6a6f3caefbee3dc68ce643d510d7
Mathew Akre and Todd Aden <mathew@>
2019-04-24 10:48:04 -0700

    add tests for random item

--- ToDoList.test/ToDoListTest.cs
+++ ToDoList.test/ToDoListTest.cs
@@ -12,11 +12,83 @@ namespace ToDoList.Tests
     {
       Bag bag = new Bag();
       bag.AddItem(Listof_Items.LongSword, 1);
-      Assert.AreEqual(1, bag.GetItem(Listof_Items.LongSword));
+      Assert.AreEqual(1, bag.CheckForItem(Listof_Items.LongSword));
+    }
+    [TestMethod]
+    public void TestAddMultipleItemsToBag()
+    {
+      Bag bag = new Bag();
+      bag.AddItem(Listof_Items.LongSword, 1);
+      bag.AddItem(Listof_Items.LongSword, 1);
+      Assert.AreEqual(2, bag.CheckForItem(Listof_Items.LongSword));
     }
 
-    
+    [TestMethod]
+    public void TestCheckForItemInBag()
+    {
+      Bag bag = new Bag();
+      bag.AddItem(Listof_Items.LongSword, 1);
+      Assert.AreEqual(1, bag.CheckForItem(Listof_Items.LongSword));
+    }
 
+    [TestMethod]
+    public void TestGetItemFromBag()
+    {
+      Bag bag = new Bag();
+      bag.AddItem(Listof_Items.LongSword, 3);
+      Assert.IsTrue(bag.GetItem(Listof_Items.LongSword));
+      Assert.IsTrue(bag.GetItem(Listof_Items.LongSword));
+      Assert.IsTrue(bag.GetItem(Listof_Items.LongSword));
+      Assert.IsFalse(bag.GetItem(Listof_Items.LongSword));
+    }
+
+
+    [TestMethod]
+    public void TestGetAllItemsFromBag()
+    {
+      Bag bag = new Bag();
+      bag.AddItem(Listof_Items.LongSword, 1);
+      bag.AddItem(Listof_Items.ShortSword, 1);
+      var list = bag.GetAllItems();
+      Assert.AreEqual(Listof_Items.LongSword, list[0].Name);
+      Assert.AreEqual(1, list[0].Quanity);
+
+      Assert.AreEqual(Listof_Items.ShortSword, list[1].Name);
+      Assert.AreEqual(1, list[1].Quanity);
+    }
+
+
+
+
+
+    [TestMethod]
+    public void TestGetRandomItemFromBag()
+    {
+      Bag bag = new Bag();
+      bag.AddItem(Listof_Items.LongSword, 1);
+      bag.AddItem(Listof_Items.ShortSword, 1);
+      bag.AddItem(Listof_Items.LargeHealthPotion, 10);
+      bag.AddItem(Listof_Items.SmallHealthPotion, 2);
+      bag.AddItem(Listof_Items.Mushrooms, 1);
+      bag.AddItem(Listof_Items.SmallHealthPotion, 4);
+      bag.AddItem(Listof_Items.TwigOfWood, 7);
+      bag.AddItem(Listof_Items.IronOre, 14);
+      bag.AddItem(Listof_Items.LogOfWood, 12);
+
+      bool foundItem = false;
+      for (int i = 0; i < 49; i++)
+      {
+        var listOfItems = bag.GetAllItems();
+        var takenItem = bag.GetRandomItem();
+
+        foreach(Listof_Items item in listOfItems)
+        {
+          if (item.Name == takenItem) foundItem = true;
+        }
+        Assert.AreEqual(true, foundItem);
+      }
+      Assert.AreEqual(listOfItems.Nothing, bag.GetRandomItem());
+    }
   }
 
 
--- ToDoList/ToDoList.cs
+++ ToDoList/ToDoList.cs
@@ -4,9 +4,10 @@ using System.Collections.Generic;
 
 //Add item to list
 //Check the list for an Item
+//Take an item from the bag
 //Combine multiple items into quanity
 //View items in the bag
-//Get a random item from the bag
+//Get a random item from the bag based on quanity of items
 //Hole in the bag loose random items
 //Have a puch for a specific item in the bag
 
@@ -35,7 +36,7 @@ namespace AdventureBag
 
 
 
-  struct BagItem
+  public class BagItem
   {
     public Listof_Items Name;
     public int Quanity;
@@ -55,10 +56,18 @@ namespace AdventureBag
 
     public void AddItem(Listof_Items name, int quanity)
     {
+       for (int i = 0; i < _Items.Count; i++)
+       {
+         if (_Items[i].Name == name)
+         {
+           _Items[i].Quanity += quanity;
+           return;
+         }
+       }
        _Items.Add(new BagItem(name, quanity));
     }
 
-    public int GetItem(Listof_Items name)
+    public int CheckForItem(Listof_Items name)
     {
       foreach(BagItem item in _Items)
       {
@@ -68,6 +77,31 @@ namespace AdventureBag
         }
       }
       return 0;
+    }
+
+    public bool GetItem(Listof_Items name)
+    {
+      for (int i = 0; i < _Items.Count; i++)
+      {
+        if (_Items[i].Name == name)
+        {
+          _Items[i].Quanity--;
+          if (_Items[i].Quanity <= 0)
+          _Items.Remove(_Items[i]);
+          return true;
+        }
+      }
+      return false;
+    }
+
+    public List<BagItem> GetAllItems()
+    {
+      return _Items;
+    }
+
+    public Listof_Items GetRandomItem()
+    {
+      return Listof_Items.Nothing;
     }
 
 
